92ef37641cb108d4b4a903816c3553ac9af6e75c
Jorge Saud <jorgelsaud@gmail.com>
2017-03-17 15:28:36 -0400

    modify header and footer

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
-
+require 'inc/customElements.php';
 function motor_child_scripts_styles() {
     wp_enqueue_style( 'motor-parent-style', get_template_directory_uri(). '/style.css' );
     wp_enqueue_style('my-adaptations-to-theme',get_stylesheet_directory_uri().'/css/adaptations.css',array('motor-less'));
---
+++ inc/customElements.php
@@ -0,0 +1,22 @@
+<?php
+function mytheme_customize_register( $wp_customize ) {
+	$wp_customize->add_setting( 'footer_image' , array(
+		'default'   => get_stylesheet_directory_uri().'/img/footer.jpg',
+		) );
+	$wp_customize->add_section( 'footer' , array(
+		'title'      => __( 'Footer Customizations', 'motor-child' ),
+		'priority'   => 30,
+		) );
+	$wp_customize->add_control(
+       new WP_Customize_Image_Control(
+           $wp_customize,
+           'logo',
+           array(
+               'label'      => __( 'Upload a image to footer backgroun', 'motor-child' ),
+               'section'    => 'footer',
+               'settings'   => 'footer_image'
+           )
+       )
+   );
+}
+add_action( 'customize_register', 'mytheme_customize_register' );
