57192d5e8f423904c75d4cf7afb1946e9a026074
Jorge Saud <jorgelsaud@gmail.com>
2017-03-17 10:47:11 -0400

    modify header and footer

--- functions.php
+++ functions.php
@@ -2,6 +2,6 @@
 
 function motor_child_scripts_styles() {
     wp_enqueue_style( 'motor-parent-style', get_template_directory_uri(). '/style.css' );
-    wp_enqueue_style('my-adaptations-to-theme',get_stylesheet_directory_uri().'css/adaptations.css');
+    wp_enqueue_style('my-adaptations-to-theme',get_stylesheet_directory_uri().'/css/adaptations.css');
 }
 add_action( 'wp_enqueue_scripts', 'motor_child_scripts_styles' );
