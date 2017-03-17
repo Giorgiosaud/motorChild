7ff7e08cc9599600046a9de61a8825a486437d21
Jorge Saud <jorgelsaud@gmail.com>
2017-03-17 10:25:53 -0400

    modify header and footer

--- footer.php
+++ footer.php
@@ -54,6 +54,61 @@ global $motor_options;
 		</div>
 
 	</div>
+	<?php if (!empty($motor_options['motor_header_topbar']) && $motor_options['motor_header_topbar']) : ?>
+<!-- TopBar - start -->
+<div class="topbar">
+	<?php
+	if (
+		!empty($motor_options['motor_header_address_ttl']) ||
+		!empty($motor_options['motor_header_address']) ||
+		!empty($motor_options['motor_header_contacts_ttl']) ||
+		!empty($motor_options['motor_header_contacts']) ||
+		!empty($motor_options['motor_header_phone_ttl']) ||
+		!empty($motor_options['motor_header_phone'])
+	) :
+	?>
+	<ul class="topbar-address">
+		<?php if (!empty($motor_options['motor_header_address_ttl']) || !empty($motor_options['motor_header_address'])) : ?>
+		<li>
+			<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/ico1.png" alt="">
+			<p>
+			<?php if (!empty($motor_options['motor_header_address_ttl'])) : ?><span><?php echo esc_attr($motor_options['motor_header_address_ttl']); ?></span><?php endif; ?> <?php if (!empty($motor_options['motor_header_address'])) echo esc_attr($motor_options['motor_header_address']); ?>
+			</p>
+		</li>
+		<?php endif; ?>
+		<?php if (!empty($motor_options['motor_header_contacts_ttl']) || !empty($motor_options['motor_header_contacts'])) : ?>
+		<li>
+			<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/ico2.png" alt="">
+			<p>
+				<?php if (!empty($motor_options['motor_header_contacts_ttl'])) : ?><span><?php echo esc_attr($motor_options['motor_header_contacts_ttl']); ?></span><?php endif; ?> <?php if (!empty($motor_options['motor_header_contacts'])) echo esc_attr($motor_options['motor_header_contacts']); ?>
+			</p>
+		</li>
+		<?php endif; ?>
+		<?php if (!empty($motor_options['motor_header_phone_ttl']) || !empty($motor_options['motor_header_phone'])) : ?>
+		<li>
+			<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/ico3.png" alt="">
+			<p>
+				<?php if (!empty($motor_options['motor_header_phone_ttl'])) : ?><span><?php echo esc_attr($motor_options['motor_header_phone_ttl']); ?></span><?php endif; ?> <?php if (!empty($motor_options['motor_header_phone'])) echo esc_attr($motor_options['motor_header_phone']); ?>
+			</p>
+		</li>
+		<?php endif; ?>
+	</ul>
+	<?php endif; ?>
+	<ul class="topbar-social">
+		<?php for ($i = 1; $i <= 10; $i++) : ?>
+			<?php if (!empty($motor_options['motor_footer_link_'.$i]) && !empty($motor_options['motor_footer_icon_'.$i])) : ?>
+				<li>
+					<a rel="nofollow" target="_blank" href="<?php echo esc_url($motor_options['motor_footer_link_'.$i]); ?>">
+						<?php echo wp_kses_post($motor_options['motor_footer_icon_'.$i]); ?>
+					</a>
+				</li>
+			<?php endif; ?>
+		<?php endfor; ?>
+	</ul>
+</div>
+<!-- TopBar - end -->
+<?php endif; ?>
+
 	<div class="copyright">
 		<p class="cont"><?php echo esc_attr($motor_options['motor_footer_copyright']); ?></p>
 	</div>
