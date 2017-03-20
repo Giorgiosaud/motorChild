e0fcafb7c36d33cce2315f1dc4dba6ca528dc599
Jorge Saud <jorgelsaud@gmail.com>
2017-03-20 18:26:58 -0400

    facebook sdk

--- header.php
+++ header.php
@@ -23,6 +23,25 @@ if (!empty($motor_options['motor_header_sticky']) && $motor_options['motor_heade
 }
 body_class($sticky_header);
 ?>>
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '614386785429381',
+      xfbml      : true,
+      version    : 'v2.8'
+    });
+    FB.AppEvents.logPageView();
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
+    
 
 <div id="page" class="site">
 <!-- Header - start -->
